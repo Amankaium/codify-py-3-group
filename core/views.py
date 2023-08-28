2f4e2d14d40e157992368dd0d5c0d79755086964
saadanbekovabegimay <137083576+saadanbekovabegimay@users.noreply.github.com>
2023-08-28 20:20:43 +0600

    create-API-product

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductsListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filterset_class = ProductFilter
--- group_project/urls.py
+++ group_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductsListAPIViews.as_view(), name='products'),
 ]
