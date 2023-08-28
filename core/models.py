f4224ba59fc057c9f1e0ac8ed2e0bce5302bfbd1
Duran07 <133371631+Duran07@users.noreply.github.com>
2023-08-28 20:07:47 +0600

    model-product

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    year = models.PositiveIntegerField()
+    genre = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
