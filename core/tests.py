a2beb482e3344fb26cfcaf04333ee9b0a2091d1d
werewolf85 <ixbox85@gmail.com>
2023-08-30 20:14:24 +0600

    modified tests

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from rest_framework.test import APITestCase
-# Create your tests here.
 def test_get_one_product(self):
     response = self.client.get(f'/product-info/{self.prod.pk}/')
     self.assertEqual(response.status_code, 200)
