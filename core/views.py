b9f6bb2e81784007246ba94320cd106e812ca21c
elverpython <elvermahmudov@gmail.com>
2023-08-30 19:25:56 +0600

    added test on API category-list

--- core/views.py
+++ core/views.py
@@ -5,6 +5,6 @@ from .serializers import *
 
 class CategoryList(APIView):
     def get(self, request):
-        cat_list= Category.objects.all()
+        cat_list = Category.objects.all()
         serializer = CategorySerializer(instance=cat_list, many=True)
         return Response(serializer.data)
