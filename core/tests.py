c74a46bc9479a7be1e2a167db612fc173ae444de
Murat0121 <murat.temirbekovich@gmail.com>
2023-08-30 19:44:14 +0600

    murat-test-category

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Category
+
+
+class CategoryTests(unittest.TestCase):
+    def setUp(self):
+        self.category = Category("Mobile")
+
+    def test_category_name(self):
+        self.assertEqual(self.category.name, "")
