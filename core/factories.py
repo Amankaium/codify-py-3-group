41aa9f3f9549558994fae565e31695b11d02f446
werewolf85 <ixbox85@gmail.com>
2023-08-30 20:55:31 +0600

    added factoryboy test

--- core/factories.py
+++ core/factories.py
@@ -1,8 +1,15 @@
 import factory
-from .models import Category
+from .models import Category, Product
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
     name = "test Category 1"
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+    name = "Test Product 1"
+    price = "200"
--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from .factories import ProductFactory
 
-def test_get_one_product(self):
-    response = self.client.get(f'/product-info/{self.prod.pk}/')
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(self.prod.name, response.data["name"])
+
+class ProductTest(APITestCase):
+    def setUp(self):
+        self.prod = ProductFactory()
+
+    def test_get_one_product(self):
+        response = self.client.get(f'/product-info/{self.prod.pk}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.prod.name, response.data["name"])
 
