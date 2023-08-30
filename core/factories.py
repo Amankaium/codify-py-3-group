26fa00f66e051b01e3b3472b968c16c405557615
saadanbekovabegimay <137083576+saadanbekovabegimay@users.noreply.github.com>
2023-08-30 21:52:46 +0600

    test api

---
+++ core/factories.py
@@ -0,0 +1,16 @@
+import factory
+from .models import Category, Product
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = "test Category 1"
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    name = factory.Sequence(lambda n: f'Product {n}')
+    price = 100
+    category = factory.SubFactory(CategoryFactory)
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .factories import ProductFactory
+class ProductTestCase(APITestCase):
+    def setUp(self):
+        self.product_1 = ProductFactory()
+        self.product_2 = ProductFactory()
+        self.product_3 = ProductFactory()
 
-# Create your tests here.
+    def test_get_list_of_3_collections(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.product_1.name, response.data[0]["name"])
+        self.assertEqual(self.product_2.name, response.data[1]["name"])
+        self.assertEqual(self.product_3.name, response.data[2]["name"])
