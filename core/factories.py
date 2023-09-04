0822bee3c2d8feb2ea4ea23e774cd5914b25a315
xdunit <xdunit@gmail.com>
2023-09-04 20:11:35 +0600

    purchase test

--- core/factories.py
+++ core/factories.py
@@ -1,17 +1,39 @@
 import factory
-from .models import Category, Product
+from .models import Category, Product, Purchase
+from django.contrib.auth.models import User
+from datetime import date
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
+
     name = "test Category 1"
+
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
+
     name = factory.Sequence(
         lambda n: f'Test Product {n}'
     )
     price = 100
     category = factory.SubFactory(CategoryFactory)
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(
+        lambda n: f'test_user_{n}'
+    )
+
+
+class PurchaseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Purchase
+
+    user = factory.SubFactory(UserFactory)
+    date = factory.LazyFunction(date.today)
--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from .models import Category
-from .factories import CategoryFactory, ProductFactory
+from .factories import CategoryFactory, ProductFactory, UserFactory, PurchaseFactory
+
+
+class CategoryListTest(TestCase):
+    def test_open_category_should_success(self):
+        response = self.client.get('/category-list/')
+        assert response.status_code == 200
+
+
+class CategoryTestCase(APITestCase):
+    def test_get_list_of_categorys(self):
+        category_1 = CategoryFactory()
+        category_2 = CategoryFactory()
+        category_3 = CategoryFactory()
+        response = self.client.get('/category-list/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(category_1.name, response.data[0]["name"])
+        self.assertEqual(category_2.name, response.data[1]["name"])
+        self.assertEqual(category_3.name, response.data[2]["name"])
 
 
 class ProductTest(APITestCase):
@@ -19,7 +37,7 @@ class TestCategoryTestCase(TestCase):
         category_name = "test category 1"
         new_category = Category.objects.create(name=category_name)
         self.assertEqual(new_category.name, category_name)
-        self.assertTrue(new_category.id > 0)      
+        self.assertTrue(new_category.id > 0)
 
 
 class ProductsTest(APITestCase):
@@ -41,3 +59,20 @@ class ProductsTest(APITestCase):
         self.assertEqual(self.prod_1.name, response.data["name"])
         self.assertEqual(self.prod_1.price, response.data["price"])
         self.assertEqual(self.prod_1.category.pk, response.data["category"])
+
+
+class PurchaseTest(APITestCase):
+    def setUp(self):
+        self.purchase_1 = PurchaseFactory()
+        self.purchase_2 = PurchaseFactory()
+        self.user = UserFactory()
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_list_of_purchases(self):
+        response = self.client.get('/purchase-list/')
+        self.assertEqual(response.status_code, 200)
+
+        data = response.data
+
+        self.assertEqual(self.purchase_1.user.id, data[0]['user'])
+        self.assertEqual(self.purchase_2.user.id, data[1]['user'])
