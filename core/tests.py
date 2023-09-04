d07a798e6020c5056ba25d08c284a48f98ff5e50
Amankaium <kaium@list.ru>
2023-09-04 19:50:01 +0600

    added model test

--- core/tests.py
+++ core/tests.py
@@ -1,4 +1,11 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from .models import Category
 
-# Create your tests here.
+
+class TestCategoryTestCase(TestCase):
+    def test_model_category_fields(self):
+        category_name = "test category 1"
+        new_category = Category.objects.create(name=category_name)
+        self.assertEqual(new_category.name, category_name)
+        self.assertTrue(new_category.id > 0)      
