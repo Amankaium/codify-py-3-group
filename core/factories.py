19d680b7588eb1a09e7334e8213eb640c8c2eb05
Murat0121 <murat.temirbekovich@gmail.com>
2023-09-04 14:39:04 +0600

    category test

---
+++ core/factories.py
@@ -0,0 +1,8 @@
+import factory
+from .models import Category
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = factory.Sequence(lambda c: f'Test category {c}')
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from .factories import CategoryFactory
+from .models import Category
+class CategoriesTest(APITestCase):
+    def setUp(self):
+        self.cate = CategoryFactory()
 
-# Create your tests here.
+    def test_get_list_of_categories(self):
+        response = self.client.get('/category-list/')
+        self.assertEqual(response.status_code, 200)
+
+
+    #def test_get_one_categorie(self):
+       # response = self.client.get(f'/category/{self.cate.pk}/')
+       # self.assertEqual(response.status_code, 200)
